08d56527060fd05512256d53cc315c5d1dabb953
Renato Crivano <renato.crivano@gmail.com>
2023-08-24 15:49:31 -0300

    corrigindo texto da home

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,6 @@ export default function Home() {
         <h1 className="display-5 fw-bold">Fórumjus</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">Forumjus é um sistema desenvolvido para receber incrições e enunciados para o Fórum de Direitos Humanos, do Tribunal Regional Federal da 2&ordf; Região.</p>
-          <p className="lead mb-4">As incrições e o enunciados são aprovados, depois votados em cada comissão e, por fim, os melhores são escolhidos em uma votação geral.</p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <a className="btn btn-primary btn-lg px-4" href="register" style={{ "color": "white" }}>Inscrição</a>
             <button type="button" className="btn btn-outline-secondary btn-lg px-4">Saiba Mais</button>
