57a54d12c5604d183a68fafc257c0015a81d1414
Baylon Aguiar <baylon@users.noreply.github.com>
2023-09-22 15:33:37 -0300

    Update index.jsx
    Alterando a pedido da Assessoria

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="px-4 py-5 my-5 text-center">
         <div className="col-lg-6 mx-auto">
         <img className='w-100' src="/saia.png" />
-          <p className="lead mb-4 mt-4">Sistema desenvolvido para receber inscrições de propostas de enunciados para a I Jornada de Direitos Humanos e Fundamentais da Justiça Federal da 2ª Região.</p>
+          <p className="lead mb-4 mt-4">Sistema desenvolvido para receber inscrição de proposta(s) de enunciado(s) para a I Jornada de Direitos Humanos e Fundamentais da Justiça Federal da 2ª Região.</p>
 
           <p className="lead mb-4 mt-4">Para mais informações, visite o <a href="https://www10.trf2.jus.br/institucional/forum-de-direitos-humanos-e-fundamentais/" target='_blank' rel="noopener">Portal do Fórum de Direitos Humanos e Fundamentais da Justiça Federal da 2ª Região</a> e leia o <a href="#regimento" onClick={()=> regimentoRef.current.show()}>Regimento da Jornada</a>.</p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
