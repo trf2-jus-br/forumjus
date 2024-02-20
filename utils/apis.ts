8d589b8afe9311921ccb8dd76bfdc4b361c7c6fb
Walace Leal Bastos Pereira <wlp@trf.net>
2024-02-20 12:29:02 -0300

    Refatoração dos endpoints para só responder a requisiçao após o 'commit' do banco de dados.

--- utils/apis.ts
+++ utils/apis.ts
@@ -82,7 +82,7 @@ export function apiHandler(handler: ApiMethodHandlers) {
             const resposta = await methodHandler({req, res, db, usuario});
 
             // caso tudo ocorra bem, salva as informações no banco.
-            db.commit();
+            await db.commit();
 
             // envia a resposta pro usuário.
             if(!res.headersSent){
@@ -90,16 +90,16 @@ export function apiHandler(handler: ApiMethodHandlers) {
             }
         } catch (err) {
             // em caso de erro, desfaz as alterações propostas pela api no banco de dados.
-            db?.rollback();
+            await db?.rollback();
 
             // tento registrar o problema ocorrido
             try{
-                db.beginTransaction();
+                await  db.beginTransaction();
                 await LogDAO.registrar(db, usuario, "Erro", JSON.stringify({
                     message: err.message,
                     stack: err.stack,
                 }));
-                db.commit();
+                await db.commit();
             }catch(err){
                 console.log("Não foi possível registrar o log.", err);
             }
