78130d7806882feeb46eb52c7a63688bcd47e9ff
Walace Leal Bastos Pereira <wlp@trf.net>
2024-07-17 12:06:06 -0300

    Testando uso de arquivos

--- pages/teste.tsx
+++ pages/teste.tsx
@@ -1,3 +1,3 @@
 export default function Teste(){
-    return <img src="http://jornada2:8081/api/uploads/158"></img>
+    return <img src="/api/uploads/c854ce07-84c2-48c6-8bfa-8dfefd9a11f6"></img>
 }
