b94e0973877e4e1c1901136b0d307ea500e2e0ff
Walace Leal Bastos Pereira <wlp@trf.net>
2024-07-24 11:38:05 -0300

    Refatoração do envio de e-mails: - Diferencia a origem baseado no ambiente;

--- utils/mailer.tsx
+++ utils/mailer.tsx
@@ -23,8 +23,6 @@ export default {
 
     transporter: nodemailer.createTransport(options),
 
-    from: process.env.SMTP_FROM,
-
     send(message) {
         return new Promise((resolve, reject)=>{
             this.transporter.sendMail(message, (err, info) => {
@@ -36,89 +34,6 @@ export default {
                 }
             })
         });
-    },
-
-    footer(electionId) {
-        return `
-
-Obrigado por utilizar o Votejus.
-
----
-VOTEJUS-${electionId}`
-    },
-
-    sendCreated(email, electionId, electionName, administratorLink) {
-        this.send({
-            from: this.from,
-            to: email,
-            subject: `${electionName}: Criada`,
-            text: `A votação "${electionName}" foi criada com sucesso.
-    
-Utilize o link abaixo para iniciar a votação e acompanhar os resultados.
-
-${administratorLink}
-
-Atenção:
-
-- Os eleitores só reberão um link por e-mail para votar quando a votação for iniciada.
-
-- Não compartilhe este link pois só o administrados da votação deve ter acesso ao link.
-
-- Não apague esse email pois sem o link acima não será possível acessar o painel de votação.` + this.footer(electionId)
-        })
-    },
-
-    sendVoteRequest(email, electionId, electionName, voterName, voterLink) {
-        this.send({
-            from: this.from,
-            to: email.trim(),
-            subject: `${electionName}: Voto Solicitado`,
-            text: `Prezado(a) ${voterName},
-
-A votação "${electionName}" foi iniciada.
-
-Utilize o link abaixo para votar sigilosamente:
-
-${voterLink}
-
-Atenção: 
-
-- Não compartilhe este link para que ninguém possa votar em seu nome.
-
-- Não apague esse email pois sem o link acima não será possível votar.
-
-- Depois de votar, não será possível trocar o voto.` + this.footer(electionId)
-        })
-    },
-
-    sendVoteAccepted(email, electionId, electionName, voterName, voterIp) {
-        this.send({
-            from: this.from,
-            to: email.trim(),
-            subject: `${electionName}: Voto Registrado`,
-            text: `Prezado(a) ${voterName},
-
-Seu voto sigiloso foi registrado para a eleição "${electionName}".` + this.footer(electionId)
-        })
-    },
-
-    sendElectionEnded(email, electionId, electionName, voterName) {
-        this.send({
-            from: this.from,
-            to: email.trim(),
-            subject: `${electionName}: Eleição Finalizada`,
-            text: `Prezado(a) ${voterName},
-
-A votação "${electionName}" foi encerrada.` + this.footer(electionId)
-        })
-    },
-
-    forumFooter(forumName) {
-        return `
-
-Atenciosamente,
-
-Equipe ${forumName}.`
     },
 
     async notificarProponente(db: PoolConnection, email: string, enunciados: any[], enunciados_reprovados: any[], nome: string){
@@ -152,7 +67,7 @@ Equipe ${forumName}.`
             }
 
             this.send({
-                from: this.from,
+                from: ambiente.EMAIL_ORGANIZACAO,
                 to: email.trim(),
                 cc: process.env.SMTP_BCC === 'false' ? undefined : ambiente.EMAIL_ORGANIZACAO,
                 subject: `${ambiente.NOME}`,
@@ -172,7 +87,7 @@ Equipe ${forumName}.`
         );
         
         this.send({
-            from: this.from,
+            from: ambiente.EMAIL_ORGANIZACAO,
             to: email.trim(),
             cc: process.env.SMTP_BCC === 'false' ? undefined : ambiente.EMAIL_ORGANIZACAO,
             subject: `${ambiente.NOME}`,
