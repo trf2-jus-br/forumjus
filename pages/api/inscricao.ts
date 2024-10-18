6080f6dfe048e28ee07ff86e0289440bb0a356e4
Walace Leal Bastos Pereira <wlp@trf.net>
2024-10-18 18:29:53 -0300

    restrinjo o acesso a dados do proponente

--- pages/api/inscricao.ts
+++ pages/api/inscricao.ts
@@ -2,9 +2,18 @@ import InscricaoDAO from "../../db/inscricao";
 import { apiHandler } from "../../utils/apis";
 
 async function listar({res, db, usuario}: API){
-    res.send(
-        await InscricaoDAO.listar(db, usuario)
-    )
+    let inscricoes = await InscricaoDAO.listar(db, usuario);
+
+    if(usuario.funcao === "ASSESSORIA" || usuario.funcao === "PROGRAMADOR"){
+        return inscricoes;
+    }
+
+    return inscricoes.map(i => ({
+        ...i,
+        attendee_document: "<SIGILOSO>",
+        attendee_phone: "<SIGILOSO>",
+        attendee_email: "<SIGILOSO>",
+    }));
 }
 
 export default apiHandler({
